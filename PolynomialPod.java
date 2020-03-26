0ec8377bb9791e43fc1bb41bae6551058f006d67
Emin Torlic <emin.torlic@icloud.com>
2020-03-26 12:05:50 -0700

    Pod Subclass

---
+++ PolynomialPod.java
@@ -0,0 +1,13 @@
+public class PolynomialPod extends Pod {
+    public PolynomialPod(String name, double... c) {
+        super(name, t -> polyFunction(c));
+    }
+
+    public static Function<Double, Double> polyFunction(double... c) {
+        Function<Double, Double>  polyFunction;
+        for (int i = 0; i < c.length; i++) {
+            polyFunction += c * Math.pow(t, i);
+        }
+        return polyFunction;
+    }
+}
